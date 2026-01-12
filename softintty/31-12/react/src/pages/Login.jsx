e22fbc02b0ac3f8ca811fc4c913d6df727bf328e
RubaALmahmoud <rubaalzoubi31@gmail.com>
2026-01-12 14:42:29 +0300

    Frontend setup: Home, Login, Axios, responsive design

---
+++ softintty/31-12/react/src/pages/Login.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Login() {
+  return (
+    <div className="container mt-5">
+      <h1>Login Page</h1>
+      <p>تسجيل الدخول هنا.</p>
+    </div>
+  );
+}
